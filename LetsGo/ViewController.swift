d8129ceef11c1307799721662d232c3c904a7a49
jackreichelt <jackreichelt@gmail.com>
2016-02-09 21:32:51 +1100

    Initial Commit

---
+++ LetsGo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LetsGo
+//
+//  Created by Jack Reichelt on 9/02/2016.
+//  Copyright (c) 2016 Jack Reichelt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
